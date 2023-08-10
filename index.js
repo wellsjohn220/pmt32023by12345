71802c735986fb2f37bce105ea511aebeead41c0
wellsjohn220 <60244641+wellsjohn220@users.noreply.github.com>
2023-08-10 10:09:53 +1000

    Update index.js

--- index.js
+++ index.js
@@ -32,20 +32,5 @@ $(document).ready(function(){
     });
   });
 })
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbxKAJFu4cNheBCr0ax6bpgr5jRXSwXOzAlst9at7ZqLie6P6ToPAiWslTHsRw72Q6NfSA/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById("msg")
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-          msg.innerHTML = "Thank you For Subscribing!"
-          setTimeout(function(){
-              msg.innerHTML = ""
-          }, 5000)
-          form.reset()
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+ 
 
